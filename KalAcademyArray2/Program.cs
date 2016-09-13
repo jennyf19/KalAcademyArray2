b05bf67e5bc8453bc29ffac4783798f5a9f520b4
Jennyf19 <ferriesj@gmail.com>
2016-09-12 17:49:12 -0700

    Binary Search algorithm O(logn)

--- KalAcademyArray2/Program.cs
+++ KalAcademyArray2/Program.cs
@@ -10,14 +10,14 @@ namespace KalAcademyArray2
     {
         static void Main(string[] args)
         {
-            int[] myArray = { -10, -1, 0, 3, 10, 11, 30, 40, 100 };
+            int[] myArray = { -10, -4, 1, 3, 4, 11, 30, 40, 100 };
             int n = myArray.Length;
-            Console.WriteLine("Fixed point is " + linearSearch(myArray, n));
+            Console.WriteLine("Fixed point is " + BinarySearch(myArray, 0, n-1));
             Console.ReadLine();
             return;
 
         }
-        public static int linearSearch(int[] myArray, int n)
+        /*public static int linearSearch(int[] myArray, int n)
         {
             int i;
             for (i = 0; i < n; i++)
@@ -25,6 +25,20 @@ namespace KalAcademyArray2
                 if (myArray[i] == i)
                     return i;
             }
+            return -1;
+        }*/
+        public static int BinarySearch(int[] myArray, int low, int high)
+        {
+            if (high >= low)
+            {
+                int mid = (low + high) / 2;
+                if (mid == myArray[mid])
+                    return mid;
+                if (myArray[mid] < mid)
+                    return BinarySearch(myArray, (mid + 1), high);
+                else
+                    return BinarySearch(myArray, low, (mid - 1));
+                        }
             return -1;
         }
     }
