9dc9626d552c1c36538f17b9047264c4867a7763
Jennyf19 <ferriesj@gmail.com>
2016-09-12 17:18:35 -0700

    Added O(n) solution

--- KalAcademyArray2/Program.cs
+++ KalAcademyArray2/Program.cs
@@ -10,6 +10,22 @@ namespace KalAcademyArray2
     {
         static void Main(string[] args)
         {
+            int[] myArray = { -10, -1, 0, 3, 10, 11, 30, 40, 100 };
+            int n = myArray.Length;
+            Console.WriteLine("Fixed point is " + linearSearch(myArray, n));
+            Console.ReadLine();
+            return;
+
+        }
+        public static int linearSearch(int[] myArray, int n)
+        {
+            int i;
+            for (i = 0; i < n; i++)
+            {
+                if (myArray[i] == i)
+                    return i;
+            }
+            return -1;
         }
     }
 }
